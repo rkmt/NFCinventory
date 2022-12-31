3bd656886f9b253bd1bbb836f0bef5c2086474f9
Jun Rekimoto <rekimoto@acm.org>
2022-12-31 21:08:17 +0900

    initial

--- html/n/settings.php
+++ html/n/settings.php
@@ -1,8 +1,8 @@
 <?php
 
-$log_sheet_id = "1dgyN_3WihKozfxS1ew6W2MpVVxMr45VYaE1VsT0223g"; // touch ID log spreadsheet ID
-$obj_sheet_id = "1QmnADZ8Sh_VhWQ0vMYRNrjaRKHtbSm0SDveUXknKmkM"; // object spreadsheet ID
-$base_url = 'https://www12384ue.sakura.ne.jp/n';
+$log_sheet_id = "<GoogleSpreadsheet ID>"; // touch ID log spreadsheet ID
+$obj_sheet_id = "<Google Spreadsheet ID>"; // object spreadsheet ID
+$base_url = 'http://<wabhost>/n';
 $sheet1_name = 'Sheet1';
 $sheet2_name = 'Sheet2';
 
